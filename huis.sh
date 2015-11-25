599d21efd7216f56f0f964189fb0f37e04a041b6
Juan Matthys Uys <opyate@gmail.com>
2015-11-25 15:02:32 +0000

    move pyenv virtualenv to brew for Darwin

--- huis.sh
+++ huis.sh
@@ -4,9 +4,3 @@
 SCRIPT=$(uname)
 echo "Running platform-specific script: $SCRIPT"
 ./$SCRIPT
-
-# pyenv post processing
-if [ ! -e /Users/juys/.pyenv/plugins/pyenv-virtualenv ] ; then
-  mkdir -p ~/.pyenv/plugins
-  git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
-fi
